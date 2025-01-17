dc0b8bd8dab32713aa2e9ce30864f5ec2f88547e
puneet93 <puneet.softweb@gmail.com>
2025-01-18 02:05:21 +0530

    Media option: Add an extra button on the “Media (preview)” page with the text “Place advertisement” and a square border, allowing users to start a campaign directly from media.

--- src/app/(dashboard)/media/components/MediaDetails.tsx
+++ src/app/(dashboard)/media/components/MediaDetails.tsx
@@ -2,13 +2,15 @@ import Preview from "@/components/Preview";
 import { X } from "lucide-react";
 import FileDetailHeader from "./FileDetailHeader";
 import FileDetailItems from "./FileDetailItems";
+import { Button } from "@/components/ui/button";
 
 export default function MediaDetails({ getValue }: { getValue?: () => void }) {
   return (
     <div className="container px-md-0 px-4 mx-auto w-full">
       <div className="shadow-container border border-adsbin-grey-100">
-        <div className="py-3.5 text-adsbin-evergreens px-5 flex items-center justify-between border-bottom border-adsbin-grey-100">
-          <h2 className="text-base font-semibold">File details</h2>
+        <div className="py-3.5 text-adsbin-evergreens px-5 flex items-center border-bottom border-adsbin-grey-100">
+          <h2 className="text-base font-semibold mr-auto">File details</h2>
+          <Button className="mr-3" variant={'outline'}>Place advertisement</Button>
           <button>
             <X color="#415B41" onClick={getValue} />
           </button>
