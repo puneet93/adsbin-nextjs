5dc44da2cf294da323b0f3fa4f3e2e42db787e3d
puneet93 <puneet.softweb@gmail.com>
2025-02-13 06:13:31 +0530

    qucik links

--- src/components/QuickLink.tsx
+++ src/components/QuickLink.tsx
@@ -9,22 +9,22 @@ export default function QuickLink(){
                 Quick Links
             </h3>
 
-            <div className="flex fle-wrap gap-2.5 items-center">
-                <div className="relative min-h-48 rounded-lg overflow-hidden anim-pulse max-w-64 w-full">
+            <div className="grid xl:grid-cols-4 2xl:grid-cols-5 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-2.5 items-center">
+                <div className="relative min-h-48 rounded-lg overflow-hidden anim-pulse">
                     <Image src="/quick-banner-image-1.png" alt="quick-banner-image-1" fill={true} className="object-center opacity-35"  />
                     <Link href={'/campaigns'} className="rounded-full py-1.5 px-5 bg-adsbin-evergreens absolute flex items-center gap-2.5 top-1/2 -translate-y-1/2 left-5 right-5">
                         <h3 className="text-white flex-grow text-base font-outfit leading-tight text-center">Start new <br />Campaign</h3>
                         <CloudUpload size={24} color="#fff" />
                     </Link>
                 </div>
-                <div className="relative min-h-48 rounded-lg overflow-hidden anim-pulse max-w-64 w-full">
+                <div className="relative min-h-48 rounded-lg overflow-hidden anim-pulse">
                     <Image src="/quick-banner-image-2.png" alt="quick-banner-image-2" fill={true} className="object-center opacity-35"  />
                     <Link href={'/media'} className="rounded-full py-1.5 px-5 bg-adsbin-evergreens absolute flex items-center gap-2.5 top-1/2 -translate-y-1/2 left-5 right-5">
                         <h3 className="text-white flex-grow text-base font-outfit leading-tight text-center">Upload new <br />Advertisement</h3>
                         <CloudUpload size={24} color="#fff" />
                     </Link>
                 </div>
-                <div className="relative min-h-48 rounded-lg overflow-hidden anim-pulse max-w-64 w-full">
+                <div className="relative min-h-48 rounded-lg overflow-hidden anim-pulse">
                     <Image src="/quick-banner-image-3.png" alt="quick-banner-image-3" fill={true} className="object-center opacity-35"  />
                     <Link href={'/approval'} className="rounded-full py-1.5 px-5 bg-adsbin-evergreens absolute flex items-center gap-2.5 top-1/2 -translate-y-1/2 left-5 right-5">
                         <h3 className="text-white flex-grow text-base font-outfit leading-tight text-center">Approve a <br />Campaign</h3>
